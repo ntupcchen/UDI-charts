92d6d87deef9e63c91b647857a855e0f156c1d4b
ntupcchen <r05922125@ntu.edu.tw>
2019-06-26 23:18:52 -0400

    Add files via upload

---
+++ basic_example_UDIuse.js
@@ -0,0 +1,55 @@
+function dynamicLoadJs(url, integrity,callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    if(integrity){
+        // script.integrity = integrity;
+        // script.crossorigin="anonymous"
+    }
+    if(typeof(callback)=='function'){
+        script.onload = script.onreadystatechange = function () {
+            if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete"){
+                callback();
+                script.onload = script.onreadystatechange = null;
+            }
+        };
+    }
+    head.appendChild(script);
+}
+
+function dynamicLoadCss(url) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.type='text/css';
+    link.rel = 'stylesheet';
+    link.href = url;
+    head.appendChild(link);
+}
+
+var UDIlocation="UDI" //folder UDI in html file location
+
+dynamicLoadJs(UDIlocation+"/UDICharts.js")
+
+dynamicLoadJs(UDIlocation+"/lib/jquery-bonsai-master/jquery.qubit.js");
+dynamicLoadJs(UDIlocation+"/lib/jquery-bonsai-master/jquery.bonsai.js");
+dynamicLoadJs(UDIlocation+"/lib/popper.min.js","sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49")
+dynamicLoadJs(UDIlocation+"/lib/bootstrap/4.1.3/js/bootstrap.min.js","sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy")
+
+
+dynamicLoadCss(UDIlocation+"/lib/bootstrap/3.3.4/css/bootstrap.min.css");
+dynamicLoadCss(UDIlocation+"/lib/jquery-bonsai-master/jquery.bonsai.css");
+dynamicLoadCss(UDIlocation+"/UDIstyle.css");
+
+$.getScript('UDI/lib/g2/3.4.10/dist/g2.min.js',function()
+{   
+    dynamicLoadJs("UDI/lib/g2/data-set/0.8.7/data-set.min.js")
+    udi = new UDICharts();
+    udi.init(inputForUDI, 'UDI');
+});
+
+
+var inputForUDI;
+var udi;
+
+
